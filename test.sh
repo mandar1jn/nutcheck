2ba969239dac82481addf75dff0a3952de105df1
Derkades <robin+github@rkslot.nl>
2023-03-15 14:11:50 +0100

    Fix civicc path

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 set -ex
 cd "$(dirname "${BASH_SOURCE[0]}")"
 make -C ../build-debug
-CIVICC="./build-debug/civicc"
+CIVICC="../build-debug/civicc"
 ./run.bash "$CIVICC" basic 2>/dev/null
 ./run.bash "$CIVICC" arrays 2>/dev/null
 ./run.bash "$CIVICC" nested_funs 2>/dev/null
